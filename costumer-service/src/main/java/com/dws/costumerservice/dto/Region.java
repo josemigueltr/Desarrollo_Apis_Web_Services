b423dd3ccce21ae038dc2443eef2337119d558bd
josemigueltr <josemigueltr@ciencias.unam.mx>
2021-05-15 22:30:30 -0500

    feature CRUD_Region add dto region

---
+++ costumer-service/src/main/java/com/dws/costumerservice/dto/Region.java
@@ -0,0 +1,14 @@
+package com.dws.costumerservice.dto;
+
+public class Region {
+
+
+    private int id;
+    private String region;
+
+    public Region(int id, String region) {
+        this.id = id;
+        this.region = region;
+    }
+
+}
